400495feb942b485797d3ef861f9c95253819773
Alexander Gehrke <github@qwertyuiop.de>
2023-08-01 16:30:05 +0200

    Use lush branch of chroma theme

--- lua/plugins/chroma.lua
+++ lua/plugins/chroma.lua
@@ -1,8 +1,8 @@
 return {
-  --'crater2150/vim-theme-chroma-lush',
   'crater2150/vim-theme-chroma',
   dev = true,
   lazy = false,
+  branch = 'lush',
   priority = 1000,
   config = function() vim.cmd.colorscheme("chroma") end
 }
