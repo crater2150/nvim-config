381fb51b80868dc1e5974a83d4318d5153131a30
Alexander Gehrke <github@qwertyuiop.de>
2023-08-28 10:36:18 +0200

    Cleanup and lockfile update

--- lua/plugins/deepl.lua
+++ lua/plugins/deepl.lua
@@ -4,7 +4,11 @@ return { 'ryicoh/deepl.vim',
 		{ '<leader><C-e>', function() vim.fn['deepl#v']("EN") end, mode = 'v' },
 		{ '<leader><C-d>', function() vim.fn['deepl#v']("DE") end, mode = 'v' },
 	},
-	dependencies = { 'tsuyoshicho/vim-pass' },
+	dependencies = {{ 'tsuyoshicho/vim-pass',
+		init = function ()
+			vim.g.pass_use_agent = 1
+		end
+	}},
 	config = function ()
 		vim.g['deepl#endpoint'] = "https://api-free.deepl.com/v2/translate"
 		vim.g['deepl#auth_key'] = vim.fn['pass#get']('web/deepl.com', 'apikey')
